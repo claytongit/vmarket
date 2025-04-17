369d6b2ca988eefd7549a4582c8641d719667446
claytongit <claytontavares61@gmail.com>
2025-04-16 22:17:36 -0300

    create supplier controller

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SupplierController;
+
+Route::controller(SupplierController::class)->group(function(){
+    Route::get('/', 'index')->name('index');
+});
 
 Route::controller(ProductController::class)->group(function(){
-    Route::get('/', 'index')->name('index');
+    Route::get('/product', 'index')->name('index');
 });
