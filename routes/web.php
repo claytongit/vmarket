2fd96883b4bb82932f4acd1685fca512553e9dee
claytongit <claytontavares61@gmail.com>
2025-04-16 22:14:32 -0300

    create product controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(ProductController::class)->group(function(){
+    Route::get('/', 'index')->name('index');
 });
